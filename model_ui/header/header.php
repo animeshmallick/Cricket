9c4b2c9000772ae400fbfad07e128dac2f64c2b1
= <animesh7995@gmail.com>
2025-02-01 16:29:05 +0530

    header

---
+++ model_ui/header/header.php
@@ -0,0 +1,8 @@
+<nav class="navbar">
+    <h2>CricketT20</h2>
+    <ul class="nav-links">
+        <li><a href="../../index.php">Home</a></li>
+        <li><a href="#">Logout</a></li>
+    </ul>
+    <div class="hamburger">&#9776;</div>
+</nav>

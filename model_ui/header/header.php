792e7729ff3d604d4be239a49bcf9f65b2efb7e6
= <animesh7995@gmail.com>
2025-02-02 01:16:19 +0530

    update nav panel

--- model_ui/header/header.php
+++ model_ui/header/header.php
@@ -2,9 +2,12 @@
     <div class="title">CricketT20</div>
     <nav class="w3-sidebar w3-bar-block w3-animate-left w3-top" style="font-size: 1rem;z-index:3;width:75%;display:none;left:0;margin: 0;padding: 0; background-image: url('../images/stadium2.png')" id="side-bar-container">
         <div class="nav-title">Controls</div>
+        <div class="separator"></div>
         <a class="nav-link" href="../home/">Home</a>
-        <a class="nav-link" onclick="w3_close()">Close</a>
         <a class="nav-link" onclick="logout()">Logout</a>
+        <div class="separator"></div>
+        <a class="nav-link" onclick="w3_close()">Close</a>
+        <div class="separator"></div>
     </nav>
     <div id='side-bar-icon' class="hamburger" onclick="w3_open()">&#9776;</div>
 </div>

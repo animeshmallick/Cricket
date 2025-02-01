3162d6b0b169bc8c80ba44f847f3daebf06023be
= <animesh7995@gmail.com>
2025-02-01 19:49:11 +0530

    redirections

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include "Common.php";
 $common = new Common();
 if($common->is_user_logged_in()){
-    $common->redirect_to('CricketT20/home/');
+    $common->redirect_to('Cricket/home/');
 }else{
 ?>
 <!DOCTYPE html>

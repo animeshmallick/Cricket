3f31c78870f4e925a194826c7e90e3b21655297a
= <animesh7995@gmail.com>
2025-02-15 21:33:53 +0530

    minor fix

--- your_bids/script.js
+++ your_bids/script.js
@@ -39,8 +39,9 @@ function fill_bid_content(bids, teams){
         card.innerHTML = `
                     <div class="card-inner">
                         <div>
-                            <div class="sub-title">Innings ${bid.innings} : Over ${bid.session === 'a' ? '1 to 6' :
-                                (bid.session === 'b' ? '7 to 10' : (bid.session === 'c' ? '11 to 16' : '17 to 20'))}</div>
+                            <div class="sub-title">${bid.type === 'session' ? `Innings ${bid.innings} : Over ${bid.session === 'a' ? '1 to 6' :
+                                (bid.session === 'b' ? '7 to 10' : (bid.session === 'c' ? '11 to 16' : '17 to 20'))}` :
+                                    bid.type === 'winner' ? 'Match Winner' : 'Special Bid'}</div>
                             <div style="display: flex;width: 100%">
                                 <div style="width: 40%">
                                     <div style="text-align: center"><p class="bid_amount">PUT <span class="amount_span_card">${bid.amount}</span></p></div>

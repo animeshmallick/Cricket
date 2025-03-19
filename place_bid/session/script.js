5a28a8ecd9d2a7c00d697cb0046c3ebf24fa75e2
= <animesh7995@gmail.com>
2025-03-19 22:56:34 +0530

    minor scrolling fix

--- place_bid/session/script.js
+++ place_bid/session/script.js
@@ -256,7 +256,8 @@ function fill_slot_details(bid_master, update_selected){
             slot_b.click();
         if (bid_master.rate_1 === max_rate)
             slot_a.click();
-        document.getElementById('bid_container').scrollIntoView({behavior: "smooth", block: "end"});
+        if(getCookie('show_tour') === null)
+            document.getElementById('bid_container').scrollIntoView({behavior: "smooth", block: "end"});
     }
     slots_time = 0;
     console.log("Slots Updated");

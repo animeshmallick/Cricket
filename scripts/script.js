cc28e04339e02889b838d08b51a40581f5b90a78
= <animesh7995@gmail.com>
2025-03-27 19:50:14 +0530

    fix srh

--- scripts/script.js
+++ scripts/script.js
@@ -219,7 +219,7 @@ function ipl_formated(team){
         return "GT";
     else if(team.includes('punjab'))
         return "PBKS";
-    else if(team.includes('hydrabad'))
+    else if(team.includes('hydrabad') || team.includes("HYDERABAD"))
         return "SRH";
     else
         return team;

f6aedd8b8fd8b9b82abcad0ac2eb80476a902071
= <animesh7995@gmail.com>
2025-04-05 01:52:24 +0530

    rate slots

--- scripts/script.js
+++ scripts/script.js
@@ -376,12 +376,12 @@ function enable_session_buttons(scorecard){
     else
         document.getElementById('b2').classList.add('disabled');
 
-    if (scorecard.balls_played > 54 && scorecard.balls_played < 90 && scorecard.innings === 2)
+    if (scorecard.balls_played > 54 && scorecard.balls_played < 90 && (scorecard.team1_score.runs - scorecard.team2_score.runs) > 15 && scorecard.team2_score.wickets < 10 && scorecard.innings === 2)
         document.getElementById('c2').classList.remove('disabled');
     else
         document.getElementById('c2').classList.add('disabled');
 
-    if (scorecard.balls_played > 90 && scorecard.over_id < 114 && (scorecard.team1_score.runs - scorecard.team2_score.runs) > 10)
+    if (scorecard.balls_played > 90 && scorecard.balls_played < 114 && (scorecard.team1_score.runs - scorecard.team2_score.runs) > 15 && scorecard.team2_score.wickets < 10 && scorecard.innings === 2)
         document.getElementById('d2').classList.remove('disabled');
     else
         document.getElementById('d2').classList.add('disabled');

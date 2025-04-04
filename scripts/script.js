e8ae675ca60b18b1b8def472bb77cf81713570b1
= <animesh7995@gmail.com>
2025-04-04 20:16:42 +0530

    minor fix

--- scripts/script.js
+++ scripts/script.js
@@ -356,7 +356,7 @@ function enable_session_buttons(scorecard){
     else
         document.getElementById('b1').classList.add('disabled');
 
-    if (scorecard.balls_played > 59 && scorecard.balls_played < 90 && scorecard.innings === 1)
+    if (scorecard.balls_played > 54 && scorecard.balls_played < 90 && scorecard.innings === 1)
         document.getElementById('c1').classList.remove('disabled');
     else
         document.getElementById('c1').classList.add('disabled');

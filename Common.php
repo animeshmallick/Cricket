a5a85a3d2a78ccc90eeffa20c443083c2a907479
= <animesh7995@gmail.com>
2025-04-09 19:45:42 +0530

    rate fix

--- Common.php
+++ Common.php
@@ -289,7 +289,7 @@ class Common
 
     public function get_winner_rates($all_bids, $amount): array
     {
-        $x = 100;
+        $x = 80;
         $a = 0.0;
         $b = 0.0;
         foreach ($all_bids as $bid) {

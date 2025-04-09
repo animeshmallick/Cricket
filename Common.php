d2e14ff5fd72f5beb9d2d54f9a5a31853899ca5c
= <animesh7995@gmail.com>
2025-04-09 20:09:07 +0530

    cover img

--- Common.php
+++ Common.php
@@ -188,7 +188,7 @@ class Common
         $r2 *= $f;
 
         if($r1 == 0 && $r2 == 0)
-            return [0.4, 0.4];
+            return [0.7, 0.7];
         return [min($r1, 1.5), min($r2, 1.5)];
     }
     public function get_unique_bid_id(string $type): int

60d36b46a37e4646200bf5af6c0ca1375be4b28f
= <animesh7995@gmail.com>
2025-04-12 22:34:05 +0530

    minor fix

--- Common.php
+++ Common.php
@@ -277,8 +277,8 @@ class Common
         $rates = $this->calculate_rates($x, $a, $b, count($all_bids), $amount, true);
 
         // TODO: Change Rate Based on Wins
-        $rates[0] = max($rates[0]+0.4, 0);
-        $rates[1] = max($rates[1]-0.4, 0);
+        $rates[0] = max($rates[0]+0.6, 0);
+        $rates[1] = max($rates[1]-0.6, 0);
 
         return $rates;
     }

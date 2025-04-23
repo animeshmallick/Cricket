9c02d0641ef3c73b9158c9256f69e16eb36c5c02
= <animesh7995@gmail.com>
2025-04-23 20:16:11 +0530

    cover imgs

--- internal/SlotScores.php
+++ internal/SlotScores.php
@@ -68,7 +68,7 @@ class Scores {
         return min(
                 max(
                     $this->get_r($r1, $r2, $slot, $this->get_balls_played($scorecard, $bid_innings)),
-            $slot == 'a' ? 45 - (3 * $curr_wkts) : ($slot == 'b' ? 69 : ($slot == 'c' ? 105 : 120))),
+            $slot == 'a' ? 45 - (3 * $curr_wkts) : ($slot == 'b' ? 50 : ($slot == 'c' ? 75 : 100))),
             $slot == 'a' ? 90 : ($slot == 'b' ? 160 : ($slot == 'c' ? 225 : 320))
         );
     }

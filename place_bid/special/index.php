f48136d2235e541899436489eb3b412381cf2e65
= <animesh7995@gmail.com>
2025-05-03 20:43:19 +0530

    fix minor

--- place_bid/special/index.php
+++ place_bid/special/index.php
@@ -35,9 +35,9 @@ if(!$common->is_user_logged_in() || !isset($_GET['room']) || $series_id == null 
             gtag('js', new Date());
             gtag('config', 'G-BQY4C789R1');
             gtag('set', {
-                'user_id': <?=$_SESSION['customer_id']?>,
-                'user_name': <?=$_SESSION['fname']?> + " " + <?=$_SESSION['lname']?>,
-                'user_type': <?=$_SESSION['user_account_type']?>,
+                'user_id': '<?=$_SESSION['customer_id']?>',
+                'user_name': '<?=$_SESSION['fname']?> <?=$_SESSION['lname']?>',
+                'user_type': '<?=$_SESSION['user_account_type']?>',
                 'browser_details': navigator.userAgent
             })
             gtag('event', 'page_view', {

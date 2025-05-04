ddaee954ac2e858fb0bb80a8c098f8ce9d1bcfdb
= <animesh7995@gmail.com>
2025-05-04 20:59:17 +0530

    fetch wrapper change

--- Common.php
+++ Common.php
@@ -624,7 +624,7 @@ class Common
         $r1 *= $f;
         $r2 *= $f;
 
-        return [min($r1, 1.1), min($r2, 1.1)];
+        return [min($r1, 1), min($r2, 1)];
     }
 
     public function get_tickets(string $ref_id)
